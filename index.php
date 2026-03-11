5eb865f4962cd55fe566c1780382a8d3c3127f23
Anabiadsr <maria.beatriz231809@gmail.com>
2026-03-11 16:30:42 -0300

    if simples em php

--- index.php
+++ index.php
@@ -1,2 +1,16 @@
 <?php
 echo "Revisão de PHP";
+
+$nome = "Ana";
+$nota = 7;
+
+if($nota >= 6) {
+    echo "Aprovado";
+}
+else{
+    echo "Reprovado";
+
+}
+for( $i =1; $i>=3; $i++){
+    echo "Rodada $i";
+}
